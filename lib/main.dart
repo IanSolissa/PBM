05288c45841e4d1decc37cbe372e9f3c84884ad5
IanSolissa <yosuasollisa@gmail.com>
2024-03-13 11:40:06 +0700

    Layout Presensi Mahasiswa

---
+++ lib/main.dart
@@ -0,0 +1,324 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(Presensi());
+}
+
+class Presensi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Absensi Mahasiswa"),
+          centerTitle: true,
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: ListView(
+          children: [
+            Container(
+              height: 2,
+              color: Colors.black,
+            ),
+            Wrap(
+              alignment: WrapAlignment.center,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.lightGreen,
+                  margin: EdgeInsets.all(20),
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.lightGreen,
+                  margin: EdgeInsets.all(20),
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.lightGreen,
+                  margin: EdgeInsets.all(20),
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.lightGreen,
+                  margin: EdgeInsets.all(20),
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.lightGreen,
+                  margin: EdgeInsets.all(20),
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.lightGreen,
+                  margin: EdgeInsets.all(20),
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.lightGreen,
+                  margin: EdgeInsets.all(20),
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.lightGreen,
+                  margin: EdgeInsets.all(20),
+                ),
+              ],
+            ),
+            Container(
+              height: 2,
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+            ListTile(
+              title: Text(
+                "Nama Mahasiswa",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text("Apakah Hari Ini Ada Kelas?"),
+              trailing: Text("10:00 AM"),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              dense: true,
+              tileColor: Colors.grey[350],
+            ),
+            Container(
+              height: 5,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
